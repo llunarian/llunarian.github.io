175386f5b2dcc2c5be64e979dbec315f3bcd6918
mjhwang <mjhwang@deali.net>
2021-01-15 14:55:04 +0900

    change image path

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,7 +2,13 @@ var store = [{
         "title": "2020년 회고록",
         "excerpt":"한줄 요약: 내가 이 짤을 올해 쓸 줄은 몰랐는데...🙊 삶이 단순해진다는 것은 많은 이점이 있다. 산만하고 복잡했던 생각도 단순해지고, 집중하고 싶은 것들에 집중할 수 있게 된다. 올해는 여러가지로 많이 단순해진 한 해였다고 생각한다. 회고 1: 왜 퇴사했나 19년에 공채로 처음 입사했던 회사. 10명이 넘는 동기들과 함께 각 부서에 배치되고, 처음엔...","categories": ["diary"],
         "tags": [],
-        "url": "https://llunarian.github.io/diary/first_post/",
+        "url": "https://llunarian.github.io/diary/2020%ED%9A%8C%EA%B3%A0%EB%A1%9D/",
+        "teaser": null
+      },{
+        "title": "2.소개",
+        "excerpt":"📌 이 시리즈는 ⟪사이트 신뢰성 엔지니어링⟫(벳시 베이어, 크리스 존스, 제니퍼 펫오프, 니얼 리처드 머피 지음, 제이펍, ISBN 979-11-88621-08-8) 의 내용을 개인적으로 학습하고 정리하기 위해 쓰여졌습니다. 따라서 해당 글을 인용 및 재배포하는 것에 대한 주의를 부탁드립니다.🚨 SRE에 대해 더 알고싶으시다면 여기 를 참고하세요! 구글 기술 운영 감독 부사장이자 SRE라는 용어를 만든...","categories": ["Site Reliability Engineering"],
+        "tags": [],
+        "url": "https://llunarian.github.io/site%20reliability%20engineering/SRE-introduce/",
         "teaser": null
       },{
         "title": "1.시작하며",
